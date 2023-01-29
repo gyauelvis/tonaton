b528341167079f6d6e9ab11ed293d0ed350323f4
Gyau Boahen Elvis <66832781+gyauelvis@users.noreply.github.com>
2023-01-29 17:04:40 +0000

    29th Jan

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,6 @@ let numberOfAdsForAllCats = 0;
 numberOfAds.forEach((ad) => {
   numberOfAdsForAllCats = numberOfAdsForAllCats + parseFloat(ad.textContent);
 });
-document.querySelector(
-  "#numberOfAllCat"
-).textContent = `${numberOfAdsForAllCats} ads`;
+document.querySelectorAll(".numberOfAllCat").forEach((cat) => {
+  cat.textContent = `${numberOfAdsForAllCats} ads`;
+});
